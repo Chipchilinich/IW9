d4be1f5f7a1804a86d4b0121cfed3fdcd619a966
Your Name <zingorskiy@gmail.ru>
2025-03-19 11:46:13 +0300

    IW9 next2

---
+++ habits/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from habits import views
+
+app_name = 'habits'
+
+router = DefaultRouter()
+router.register(r"habits", views.HabitViewSet, basename="habits")
+
+urlpatterns = [
+    path("public_habits/", views.PublicHabitListApiView.as_view(), name="public_habits"),
+    path("home/", views.IndexView.as_view(), name="home"),
+
+] + router.urls
